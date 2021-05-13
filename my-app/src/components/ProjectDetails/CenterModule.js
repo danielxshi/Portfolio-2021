c41eb051572d25dc19ffda217f06774e208eac94
Daniel <xdshi@sfu.ca>
2021-05-13 02:50:26 -0700

    Adjusted layout to anchor -right- for Next Project section

--- my-app/src/components/ProjectDetails/CenterModule.js
+++ my-app/src/components/ProjectDetails/CenterModule.js
@@ -2,8 +2,8 @@ function CenterModule(props) {
     return (
         <section className="team-section sect-last sect-1">
             <div className="grid-row-space grid-container">
-                <h2 className="h2-anchor-left rel-margin-bot">{props.title} <strong className="highlight-text">_</strong></h2>
                 <div className="center-module ">
+                    <h2 className="rel-margin-bot">{props.title} <strong className="highlight-text">_</strong></h2>
                     <p className="rel-margin-bot">{props.text}</p>
                     <p className="rel-margin-bot"><strong>{props.org}</strong></p>
                     <a href={props.href} className="primary-btn black-border-btn">
